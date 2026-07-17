fd9e18baef3da8f0e44ca4992dc10b3f912d6f9b
saazidrehann26 <itssrehann2324@gmail.com>
2026-07-17 22:06:15 +0600

    Add program to print multiplication table

---
+++ printing_table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter value of n:");
+    scanf("%d",&n);
+    printf("Table is:%d\n",n);
+    for(i=1;i<=10;i++){
+        printf("%dx%d=%d\n",n,i,n*i);
+    }
+    return 0;
+}
