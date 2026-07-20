63c1a60e30420ed6b00e798684e4161105d2ee2d
Sazid Rehan <itssrehann2324@gmail.com>
2026-07-20 23:46:06 +0600

    Modify star printing to use user-defined matrix size
    Updated the program to allow user input for matrix size.User will give value of n.

--- printing star in a matrix (row coloum )form.c
+++ printing star in a matrix (row coloum )form.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,j;
-    for(i=1;i<=10;i++){
-            for(j=1;j<=10;j++){
+    int i,j,n;
+    printf("Enter value of n:");
+        scanf("%d",&n);
+    for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
                 printf("*");
             }
             printf("\n"); /*we need to put next line here 
